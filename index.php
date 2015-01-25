e2c00df6b70a9d4dba3e2daf3d4335cd85b4ef63
Spencer Palmer <palmersp@spencerpalmer.com>
2015-01-24 23:25:12 -0700

    updating index

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<head>
+	<title>Spencer Palmer</title>
+</head>
+<body>
+	<header>
+		<h1>Welcome</h1>
+		<nav>
+			<ul>
+				<li><a href="/assignments/">Assignments</a>
+					<ul>
+						<li><a href="/assignments/survey.php">Survey</a></li>
+					</ul>
+				</li>
+			</ul>
+		</nav>
+	</header>
+	<main>
+
+	</main>
+	<footer>
+
+	</footer>
+
+</body>
