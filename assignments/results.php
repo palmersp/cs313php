f0e95c8efd4d9f5bbfe32a96d0e4d20bb4826c65
Spencer Palmer <palmersp@spencerpalmer.com>
2015-01-25 20:58:17 -0700

    update of assignments folder

---
+++ assignments/results.php
@@ -0,0 +1,40 @@
+<?php 
+
+	session_start();
+	$_SESSION['vote'] = TRUE;
+
+ ?>
+
+ <!DOCTYPE html>
+ <html>
+ <head>
+ 	<title>Results</title>
+ </head>
+ <body>
+ 	<header>
+		<h1>Survey</h1>
+		<nav>
+			<ul>
+				<li><a href="/">Home</a></li>
+				<li><a href="/assignments/">Assignments</a>
+					<ul>
+						<li><a href="/assignments/survey.php">Survey</a></li>
+					</ul>
+				</li>
+			</ul>
+		</nav>
+	</header>
+	<main>
+
+
+ 	<?php 
+
+ 	echo $_POST['iceCream'];
+ 	echo $_POST['seasons'];
+ 	echo $_POST['building'];
+ 	echo $_POST['subject'];
+
+ 	 ?>
+ 	</main>
+ </body>
+ </html>
