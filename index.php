919ea96df8a3bbd07f57cd1b7b48288dddf4f72b
Spencer Palmer <palmersp@spencerpalmer.com>
2015-01-27 08:57:53 -0700

    adding styles

--- index.php
+++ index.php
@@ -1,25 +1,46 @@
 <!DOCTYPE html>
-<head>
-	<title>Spencer Palmer</title>
-</head>
-<body>
-	<header>
-		<h1>Welcome</h1>
-		<nav>
-			<ul>
-				<li><a href="/assignments/">Assignments</a>
-					<ul>
-						<li><a href="/assignments/survey.php">Survey</a></li>
-					</ul>
-				</li>
-			</ul>
-		</nav>
-	</header>
-	<main>
-
-	</main>
-	<footer>
-
-	</footer>
-
-</body>
+<html>
+    <head lang="en">
+        <meta charset="UTF-8">
+        <title>Home</title>
+        <meta name="author" content="Spencer Palmer">
+        <meta name="description" content="Personal portfolio website of Spencer Palmer">
+        <link href="/css/screen.css" type="text/css" rel="stylesheet" media="screen">
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+        <?php include $_SERVER['DOCUMENT_ROOT'] . '/modules/head.php'; ?>
+    </head>
+    <body>
+        <div class="site_container clearfix">
+        <header class="clearfix" role="banner">
+            <div class="container header">
+                <?php include $_SERVER['DOCUMENT_ROOT'] . '/modules/header.php'; ?>
+              
+                <nav class="main_nav clearfix" role="navigation">
+                    <?php include $_SERVER['DOCUMENT_ROOT'] . '/modules/main_nav.php'; ?>
+                </nav>
+            </div>
+        </header>
+        <main role="main">
+            <div class="container main clearfix">
+                <?php include $_SERVER['DOCUMENT_ROOT'] . '/modules/main.php'; ?>
+                <h2>Welcome</h2>
+                <picture>
+                    <img class="container" srcset="/media/images/portfolioPic600.png" title="Spencer Palmer" alt="Spencer Palmer">
+                </picture>
+            </div>   
+         </main>
+        
+        
+            <footer class="clearfix" role="contentinfo">
+            <div class=" container footer clearfix">
+                <?php include $_SERVER['DOCUMENT_ROOT'] . '/modules/footer.php'; ?>
+            
+                <?php
+                // outputs e.g. 'Last modified: March 04 1998 20:43:59.'
+                echo "Last modified: " . date ("F d Y H:i:s.", getlastmod());
+                ?>
+            </div>
+        </footer>
+        </div>
+    </body>
+</html>
