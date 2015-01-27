fd80336e9440d528bfc9ae4f8e24ea3c637a223d
Spencer Palmer <palmersp@spencerpalmer.com>
2015-01-27 08:57:13 -0700

    modularizing

---
+++ modules/header.php
@@ -0,0 +1,9 @@
+<h1>
+    <a href="/">
+        <img src="/media/images/portfolioLogo.png" title="Logo " alt="logo"> 
+    </a>
+</h1>
+        
+            
+        
+
---
+++ modules/main_nav.php
@@ -0,0 +1,5 @@
+<ul>
+    <li><a class="hollow button" href="#">Design</a></li>
+    <li><a class="hollow button" href="/assignments">Assignments</a></li>
+    <li><a class="hollow button" href="#">About</a></li>
+</ul>
